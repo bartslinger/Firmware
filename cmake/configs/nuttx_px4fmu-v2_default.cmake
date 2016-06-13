772a1b70f62e5239611a576a9efc3ff9f0030acf
Bart Slinger <bartslinger@gmail.com>
2016-06-13 15:31:15 +0200

    indi attitude files copied from pid

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -94,6 +94,7 @@ set(config_module_list
 	modules/fw_pos_control_l1
 	modules/fw_att_control
 	modules/mc_att_control
+    modules/mc_indi_att_control
 	modules/mc_pos_control
 	modules/vtol_att_control
 
